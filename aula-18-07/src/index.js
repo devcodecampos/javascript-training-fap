b7c0f7c2d9760ca4725a5c82169406b163b45d7a
Matheus Campos <devcodecampos@gmail.com>
2023-07-19 18:12:59 -0300

    add EvenOdd exercise

--- aula-18-07/src/index.js
+++ aula-18-07/src/index.js
@@ -1,33 +1,40 @@
 const prompt = require('prompt-sync')()
 const mean = require('./utils/mean')
+const isEven = require('./utils/even-odd')
 
 let option
 do {
-  console.log(`
-MATEMÁTICA
-
-(4) Média Aritmética
-(0) Sair`)
-
+  console.log(`\nMATEMÁTICA \n(1) Par ou Ímpar \n(4) Média Aritmética \n(0) Sair`)
   option = Number.parseInt(prompt('Escolha uma opção: '))
-  console.log(option)
 
   switch (option) {
+    case 1:
+      let optionEvenOdd
+      do {
+        console.log(`\nPar ou Ímpar \n(1) Verificar número  \n(0) Voltar`)
+
+        optionEvenOdd = Number.parseInt(prompt('Escolha uma opção: '))
+        if (optionEvenOdd === 1) {
+          let num = Number.parseInt(prompt('Informe um número: '))
+          if (isEven(num)) {
+            console.log(`O número ${num} é PAR`)
+          } else {
+            console.log(`O número ${num} é ÍMPAR`)
+          }
+        } else if (optionEvenOdd !== 0) {
+          console.log('Informe uma opção disponível')
+        }
+      } while (optionEvenOdd !== 0)
+      break
     case 4:
       let arrayNumbers = []
-      let option
       do {
-        console.log(`
-        \nMÉDIA ARITMÉTICA - Número escolhidos:[${arrayNumbers}]
-        (1) Informar número
-        (2) Calcular
-        (0) Voltar
-        `)
+        console.log(`\nMÉDIA ARITMÉTICA - Número escolhidos:[${arrayNumbers}] \n(1) Informar número \n(2) Calcular \n(0) Voltar`)
+        optionMean = Number.parseInt(prompt('Escolha uma opção: '))
 
-        option = Number.parseInt(prompt('Escolha uma opção: '))
-        if (option === 1) {
+        if (optionMean === 1) {
           arrayNumbers.push(Number.parseInt(prompt('Informe um número: ')))
-        } else if (option === 2) {
+        } else if (optionMean === 2) {
           if (!arrayNumbers.length) {
             console.log('Nenhum valor foi informado')
             continue
@@ -38,11 +45,9 @@ MATEMÁTICA
         } else if (option !== 0) {
           console.log('Informe uma opção disponível')
         }
-      } while (option !== 0)
+      } while (optionMean !== 0)
       break
     default:
       console.log('Informe uma opção disponível')
   }
-
-
 } while (option !== 0)

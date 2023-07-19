3baf40bf440b75f0d10b878064adfb8869db9e5a
Matheus Campos <devcodecampos@gmail.com>
2023-07-19 18:56:02 -0300

    add comparison exercise

--- aula-18-07/src/index.js
+++ aula-18-07/src/index.js
@@ -1,17 +1,19 @@
 const prompt = require('prompt-sync')()
-const mean = require('./utils/mean')
+
 const isEven = require('./utils/even-odd')
+const isGreater = require('./utils/greater-number')
+const mean = require('./utils/mean')
 
 let option
 do {
-  console.log(`\nMATEMÁTICA \n(1) Par ou Ímpar \n(4) Média Aritmética \n(0) Sair`)
+  console.log(`\nMATEMÁTICA \n(1) Par ou Ímpar \n(2) Comparar qual número é maior entre dois valores informados \n(4) Média Aritmética \n(0) Sair`)
   option = Number.parseInt(prompt('Escolha uma opção: '))
 
   switch (option) {
     case 1:
       let optionEvenOdd
       do {
-        console.log(`\nPar ou Ímpar \n(1) Verificar número  \n(0) Voltar`)
+        console.log(`\nPar ou Ímpar \n(1) Verificar número \n(0) Voltar`)
 
         optionEvenOdd = Number.parseInt(prompt('Escolha uma opção: '))
         if (optionEvenOdd === 1) {
@@ -26,6 +28,26 @@ do {
         }
       } while (optionEvenOdd !== 0)
       break
+    case 2:
+      let optionGreater
+      do {
+        console.log(`\nComparar qual número é maior entre dois valores informados \n(1) Informar números \n(0) Voltar`)
+        optionGreater = Number.parseInt(prompt('Escolha uma opção: '))
+
+        if (optionGreater === 1) {
+          let firstNumber = Number.parseInt(prompt('Informe o primeiro número: '))
+          let secondNumber = Number.parseInt(prompt('Informe o segundo número: '))
+          
+          if (firstNumber === secondNumber) {
+            console.log(`Os números informados são iguais`)
+          } else {
+            console.log(`O número maior é: ${isGreater(firstNumber, secondNumber)}`)
+          }
+        } else if (optionGreater !== 0) {
+          console.log('Informe uma opção disponível')
+        }
+      } while (optionGreater !== 0)
+      break
     case 4:
       let arrayNumbers = []
       do {
@@ -33,7 +55,7 @@ do {
         optionMean = Number.parseInt(prompt('Escolha uma opção: '))
 
         if (optionMean === 1) {
-          arrayNumbers.push(Number.parseInt(prompt('Informe um número: ')))
+          arrayNumbers.push(Number.parseFloat(prompt('Informe um número: ')))
         } else if (optionMean === 2) {
           if (!arrayNumbers.length) {
             console.log('Nenhum valor foi informado')
---
+++ aula-18-07/src/utils/greater-number.js
@@ -0,0 +1,9 @@
+function isGreater(firstNumber, secondNumber){
+    if(firstNumber > secondNumber){
+        return firstNumber
+    } else {
+        return secondNumber
+    }
+}
+
+module.exports = isGreater

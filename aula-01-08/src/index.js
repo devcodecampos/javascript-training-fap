ea5f6d413a3bc0b3545f3c0f5d3e3201cf27376a
Matheus Campos <devcodecampos@gmail.com>
2023-08-03 00:30:23 -0300

    modify the "age" attribute of the Person class to private and add getters and setters methods to access and change the age value

--- aula-01-08/src/index.js
+++ aula-01-08/src/index.js
@@ -3,8 +3,9 @@ import Student from "./models/student.mjs"
 
 const person1 = new Person('matheus', 27)
 console.log(person1)
-
 console.log(person1.introduceSelf())
 
 const student1 = new Student('devcodecampos', 27, 'fap-20')
 console.log(student1)
+
+console.log(person1.age)
--- aula-01-08/src/models/person.mjs
+++ aula-01-08/src/models/person.mjs
@@ -1,10 +1,20 @@
 export default class Person {
+  #age
+
   constructor(name, age) {
     this.name = name
-    this.age = age
+    this.#age = age
   }
 
-  introduceSelf(){
-    return `name: ${this.name}, age: ${this.age}`
+  setAge(age) {
+    this.#age = age
+  }
+
+  getAge() {
+    return this.#age
+  }
+
+  introduceSelf() {
+    return `name: ${this.name}, age: ${this.#age}`
   }
 }

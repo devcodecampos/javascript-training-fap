6fc9abfdc800086296878916511f88089cfe4442
devcodecampos <devcodecampos@gmail.com>
2023-08-17 20:49:55 -0300

    #class-15-08 refactor function that finds the most frequent value in an array of numbers

--- aula-15-08/src/arrays/find-most-frequent-value.ts
+++ aula-15-08/src/arrays/find-most-frequent-value.ts
@@ -1,13 +1,39 @@
-export function findMostFrequentValue(arr: number[]): {} {
-  const frequency: { [key: number]: number } = {};
+interface Frequency {
+  value: number;
+  repetitions: number;
+}
 
-  for (let num of arr) {
-    if (frequency[num] === undefined) {
-      frequency[num] = 1;
-    } else {
-      frequency[num] += 1;
-    }
+export function findMostFrequentValue(arr: number[]): Frequency[] {
+  const frequencyNumbers: Frequency[] = [];
+  const uniqueArr: number[] = [...new Set(arr)];
+
+  for (const num of uniqueArr) {
+    frequencyNumbers.push({
+      value: num,
+      repetitions: 0,
+    });
   }
 
-  return frequency;
+  frequencyNumbers.map((element) => {
+    for (const num of arr) {
+      if (num === element.value) {
+        element.repetitions += 1;
+      }
+    }
+
+    return element;
+  });
+
+  const highestNumberOfRepetitions: number = frequencyNumbers.reduce(
+    (acc, cur) => {
+      return Math.max(acc, cur.repetitions);
+    },
+    0
+  );
+
+  const highestNumber: Frequency[] = frequencyNumbers.filter(
+    (element) => element.repetitions === highestNumberOfRepetitions
+  );
+
+  return highestNumber;
 }
